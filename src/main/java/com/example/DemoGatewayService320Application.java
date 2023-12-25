ece0a9365501e3a828eee001dbcfa47d82f488db
Francis <franko200ng@gmail.com>
2023-12-25 15:54:28 +0100

    first commit

---
+++ src/main/java/com/example/DemoGatewayService320Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGatewayService320Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGatewayService320Application.class, args);
+	}
+
+}
